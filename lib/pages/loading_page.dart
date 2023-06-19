1471fbfdd3408dfcb98d027246560baab68b33ec
pf-haidar <pedrohfilho.dev@gmail.com>
2023-06-19 15:46:55 -0300

    first commit

---
+++ lib/pages/loading_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class LoadingPage extends StatelessWidget {
+  const LoadingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              'Carregando...',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
